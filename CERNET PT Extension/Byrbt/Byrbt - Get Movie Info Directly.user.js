a2eb23667a5e61641a4f629301d7cf464a513047
Rhilip <rhilipruan@gmail.com>
2017-10-27 23:14:56 +0800

    Update

--- CERNET PT Extension/Byrbt/Byrbt - Get Movie Info Directly.user.js
+++ CERNET PT Extension/Byrbt/Byrbt - Get Movie Info Directly.user.js
@@ -9,8 +9,7 @@
 // @connect     api.douban.com
 // @connect     api.bgm.tv
 // @grant       GM_xmlhttpRequest
-// @grant       GM_setClipboard
-// @version     20171021
+// @version     20171027
 // ==/UserScript==
 
 var script_version = '';
@@ -22,57 +21,87 @@ if (GM_info && GM_info.script) {
 var cat = parseInt(location.href.match(/(\d+)$/)[1]); // 408,电影 ; 401,剧集 ; 404, 动漫
 
 function limit_item(raw_str, limit){
-    var _str = raw_str.split(" / ");
-    return _str.slice(0,limit).join("/");
+    limit = limit || 3;
+    var _str = raw_str.replace(/ \/ /g,"/");  // 统一
+    return _str.split("/").slice(0,limit).join("/");  // 分割，切片，组合
 }
 
-function format_movie(raw){
-    return '<br />\n' +
-        '<fieldset style="font-family: Consolas;">\n' +
-        '\t<legend><span style="color:#ffffff;background-color:#000000;">&nbsp;海报&nbsp;</span></legend>请在此处<span style="color:#ff0000;">上传图片</span>。</fieldset>\n' +
-        '<br />\n' +
-        '<fieldset style="font-family: Consolas;">\n' +
-        '\t<legend><span style="color:#ffffff;background-color:#000000;">&nbsp;简介&nbsp;</span></legend>' + raw + '</fieldset>\n' +
-        '<br />\n' +
-        '<fieldset style="font-family: Consolas;">\n' +
-        '\t<legend><span style="color:#ffffff;background-color:#000000;">&nbsp;iNFO&nbsp;</span></legend>请在此处<span style="color:#ff0000;">替换为电影的Mediainfo信息</span>。  </fieldset>\n' +
-        '<br />\n';
-}
-
-$(document).ready(function() {
+CKEDITOR.on('instanceReady', function (evt) {
     // 构造本脚本和用户交互行
-    $('#compose').find('> table > tbody > tr:eq(2)').after('<tr id="ben_help"><td class="rowhead nowrap">快速填写信息</td><td class="rowfollow" valign="top" align="left"><input type="text" id="ben_url" placeholder="相应网站上资源信息页的 URL" size="80"> 简介美化：<input type="checkbox" id="ben_format" checked="checked"> <input type="button" id="ben_btn" value="搜索/导入">&nbsp;&nbsp;<span id="ben_info"></span><br> 此功能可以从 豆瓣 / Bangumi 上抓取信息，并生成标题tag信息（在正确类型下）及简介。目前仅支持电影 / 剧集 / 动漫区。（如有问题，请带上链接和错误信息及时<a href="https://bt.byr.cn/sendmessage.php?receiver=222616" target="_blank"><img class="button_pm" src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA=="></a>）<br><span id="ben_extra" style="display:none"></span></td></tr>');
+    $('#compose').find('> table > tbody > tr:eq(2)').after('<tr id="ben_help"><td class="rowhead nowrap">快速填写信息</td><td class="rowfollow" valign="top" align="left"><input type="text" id="ben_url" placeholder="相应网站上资源信息页的 URL" size="80"> <input type="button" id="ben_btn" value="搜索/导入">&nbsp;<input type="button" id="ben_format" value="简介美化/辅助填写">&nbsp;&nbsp;<span id="ben_info"></span><br> 此功能可以从 豆瓣 / Bangumi 上抓取信息，并生成标题tag信息（在正确类型下）及简介。目前仅支持电影 / 剧集 / 动漫区。（如有问题，请带上链接和错误信息及时<a href="https://bt.byr.cn/sendmessage.php?receiver=222616" target="_blank"><img class="button_pm" src="data:image/webp;base64,UklGRhoAAABXRUJQVlA4TA0AAAAvAAAAEAcQERGIiP4HAA=="></a>）<br><span id="ben_extra"><hr><span>你也可以手动从简介生成工具，如： <a href=\'http://movieinfogen.sinaapp.com/\' target=\'_blank\'>http://movieinfogen.sinaapp.com/ (需要豆瓣账号)</a>， <a href=\'http://huancun.org/\' target=\'_blank\'>http://huancun.org/</a> 获取数据并填入，然后点击`简介美化`。</span></span></td></tr>');
 
+    // 注册脚本添加的相关tag
     var ben_info = $("#ben_info");
-    var ben_format_btn  = $("#ben_format");
     var ben_extra = $("#ben_extra");
 
-    function meet_error(){
-        ben_extra.html("<hr>在遇到API未正确返回数据的情况下，请使用其他简介生成工具作为替代，如： <a href='http://movieinfogen.sinaapp.com/' target='_blank'>http://movieinfogen.sinaapp.com/</a>， <a href='http://huancun.org/' target='_blank'>http://huancun.org/</a>。<span id='ben_error_format'>并在填入信息后点击此处 -><a id='ben_error_format_btn'>美化电影简介</a></span>").show();
-        if (cat === 408){
-            $("a#ben_error_format_btn").click(function(){
-                var descr = CKEDITOR.instances.descr.getData();
-                if (descr.match(/译　　名　(.+?)</)) {
-                    $("input[name$=cname]").val(descr.match(/译　　名　(.+?)</)[1]);  // 填写中文名
-                }
-                if (descr.match(/豆瓣链接　(.+?)</)) {
-                    $("input[name=dburl]").val(descr.match(/豆瓣链接　(.+?)</)[1]);  // 豆瓣链接
-                }
-                if (descr.match(/IMDb链接　(.+?)</)) {
-                    $("input[name=url]").val(descr.match(/IMDb链接　(.+?)</)[1]);  // IMDb链接
-                }
-                if (descr.match(/类　　别　(.+?)</)) {
-                    $("#movie_type").val(descr.match(/类　　别　(.+?)</)[1]);  // 电影类别
-                }
-                if (descr.match(/产　　地　(.+?)</)) {
-                    $("#movie_country").val(descr.match(/产　　地　(.+?)</)[1]);  // 制片国家/地区
-                }
-                CKEDITOR.instances.descr.setData(format_movie(descr));
-            });
-        } else {
-            $("span#ben_error_format").hide();
+    function ben_input_help(){   // 表单辅助填写
+        var descr = CKEDITOR.instances.descr.getData();
+        if (descr.match(/译　　名　(.+?)</)) {  // 填写中文名
+            $("input[name$=cname]").val(limit_item(descr.match(/译　　名　(.+?)</)[1]));
+        }
+        if (descr.match(/(https?:\/\/movie\.douban\.com\/subject\/\d+\/?)/)) {  // 豆瓣链接
+            $("input[name=dburl]").val(descr.match(/(https?:\/\/movie\.douban\.com\/subject\/\d+\/?)/)[1]);
+        }
+        if (descr.match(/(https?:\/\/www\.imdb\.com\/title\/tt\d+\/?)/)) {  // IMDb链接
+            $("input[name=url]").val(descr.match(/(https?:\/\/www\.imdb\.com\/title\/tt\d+\/)/)[1]);
+        }
+        if (descr.match(/类　　别　(.+?)</)) {  // 电影类别
+            $("#movie_type").val(limit_item(descr.match(/类　　别　(.+?)</)[1]));
+        }
+        if (descr.match(/产　　地　(.+?)</)) {  // 制片国家/地区
+            $("#movie_country").val(limit_item(descr.match(/产　　地　(.+?)</)[1]));
         }
     }
+
+    function ben_descr_format(){   // 简介美化
+        var descr = CKEDITOR.instances.descr.getData();
+
+        if (!descr.match("byrbt_info_gen")){   // 如果没有发现`Gen`信息
+            if (cat === 408){   // 电影区
+                descr = descr || "请在此处<span style=\"color:#ff0000;\">填写简介</span>。";
+                descr = '<br />\n' +
+                    '<fieldset style="font-family: Consolas;">\n' +
+                    '\t<legend><span style="color:#ffffff;background-color:#000000;">&nbsp;海报&nbsp;</span></legend>请在此处<span style="color:#ff0000;">上传图片</span>。</fieldset>\n' +
+                    '<br />\n' +
+                    '<fieldset style="font-family: Consolas;">\n' +
+                    '\t<legend><span style="color:#ffffff;background-color:#000000;">&nbsp;简介&nbsp;</span></legend>' + descr + '</fieldset>\n' +
+                    '<br />\n' +
+                    '<fieldset style="font-family: Consolas;">\n' +
+                    '\t<legend><span style="color:#ffffff;background-color:#000000;">&nbsp;iNFO&nbsp;</span></legend>请在此处<span style="color:#ff0000;">替换为电影的Mediainfo信息</span>。  </fieldset>\n' +
+                    '<br />\n';
+            } else if (cat === 401) {  // 剧集区
+                // 暂无QAQ
+            } else if (cat === 404) {  // 动漫区
+                if (descr) {
+                    descr = descr.replace(/\[b](.+?)\[\/b]/g,"<span style=\"color: #008080;font-family: Impact,serif;font-size: large;\"> $1 </span><br>");
+                } else {  // TODO 似乎没有用.......
+                    descr = '<br />\n' +
+                        "<span style=\"color: #008080;font-family: Impact,serif;font-size: large;\">STORY : </span><br>" +
+                        '<div id="descr_story"><br /><br /><br /><br /></div><br />\n' +
+                        "<span style=\"color: #008080;font-family: Impact,serif;font-size: large;\">STAFF : </span><br>" +
+                        '<div id="descr_staff"><br /><br /><br /><br /></div><br />\n' +
+                        "<span style=\"color: #008080;font-family: Impact,serif;font-size: large;\">CAST : </span><br>" +
+                        '<div id="descr_cast"><br /><br /><br /><br /></div><br />\n';
+                }
+            }
+
+            // 对简介中出现的图片字符串进行标红处理
+            descr = descr.replace(/^(http.+?\.jpg)/,"<span style=\"color:#ff0000;\">$1</span>");
+
+            // 添加`Gen`信息，以防止二次格式化
+            descr += '<div class=\"byrbt_info_gen\" data-version=\"' + script_version + '\" style=\"display:none\">Info Format Powered By @Rhilip</div>';
+
+            CKEDITOR.instances.descr.setData(descr);
+            ben_info.text("简介美化完成。");
+        } else {
+            ben_info.text("简介已被美化，请勿再次美化。");
+        }
+    }
+
+    $("#ben_format").click(function(){
+        ben_descr_format();  // 格式化
+        ben_input_help();    // 辅助填写表单
+    });
 
     $('#ben_btn').click(function () {
         var subject_url = $('#ben_url').val().trim();
@@ -92,35 +121,12 @@ $(document).ready(function() {
                         var resj = JSON.parse(res.responseText);  // 解析成Json格式
                         if (resj.success) {
                             var raw = resj.format;
-                            raw = raw.replace(/\n/g, "<br />");   // 将原始字符串（为BBCode格式）中的`\n` 替换为 `<br>`
-                            var descr = raw;
+                            var descr = raw.replace(/\n/g, "<br />");  // 将原始字符串（为BBCode格式）中的`\n` 替换为 `<br>`
 
-                            $("input[name$=cname]").val(limit_item(resj.title,3));  // 填写中文名(限制最多三个)
-                            $("input[name=url]").val(resj.imdb_link);  // 豆瓣链接
-                            $("input[name=dburl]").val(resj.douban_link);  // IMDb链接
-                            if (cat === 408){   // 电影区
-                                // 填写标题项
-                                $("#movie_type").val(limit_item(resj.genres,3));  // 电影类别(限制最多三个)
-                                $("#movie_country").val(limit_item(resj.countries,3));   // 制片国家/地区(限制最多三个)
-
-                                // 剧集区格式化选项
-                                if (ben_format_btn.prop('checked')) {
-                                    descr = format_movie(descr);
-                                }
-                            } else if (cat === 401) {  // 剧集区
-                                // 暂无QAQ
-                            } else if (cat === 404) {  // 动漫区
-                                // 填写标题项
+                            // 这里是不能从简介部分中直接得出的信息
+                            if (cat === 404) {  // 动漫区
                                 $("input[name=comic_year]").val(resj.air_date);
-
-                                // 动漫区格式化
-                                if (ben_format_btn.prop('checked')) {
-                                    descr = descr.replace(/\[b](.+?)\[\/b]/g,"<span style=\"color: #008080;font-family: Impact,serif;font-size: large;\"> $1 </span><br>");
-                                }
                             }
-
-                            // 对简介中出现的图片字符串进行标红处理
-                            descr = descr.replace(/^(http.+?\.jpg)/,"<span style=\"color:#ff0000;\">$1</span>");
 
                             // 添加图片信息
                             if (resj.img) {
@@ -133,23 +139,18 @@ $(document).ready(function() {
                                 ben_extra.html(img_html).show();
                             }
 
-                            // 添加`Gen`信息
-                            descr += '<div class=\"byrbt_info_gen\" data-url=\"' + subject_url + '\" data-version=\"' + script_version + '\" style=\"display:none\">' + resj.copyright +  '</div>';
-
-                            CKEDITOR.instances.descr.setData(descr);
-                            ben_info.text("已完成填写，你也可以使用`Ctrl + V`粘贴简介部分内容原始源码。");
+                            CKEDITOR.instances.descr.setData(descr);  // 填入原始简介
+                            $("#ben_format").click();   // 模拟点击，美化简介及辅助填写表单
+                            ben_info.text("已完成填写。");
                         } else {
                             ben_info.text("失败了欸，原因：" + resj.error);
-                            meet_error();
                         }
                     } else {
                         ben_info.text("似乎咩从服务器返回到正确的数据，错误号：" + res.status);
-                        meet_error();
                     }
                 },
                 onerror: function () {
                     ben_info.text("你在外太空吗？连服务器娘都不搭理你。");
-                    meet_error();
                 }
             });
         } else {
@@ -234,6 +235,7 @@ $(document).ready(function() {
 
 /**
  * Created by Rhilip on 10/12/2017.
+ * 20171027: 大幅度修改脚本，允许直接使用其他简介生成工具提供的简介，并美化及辅助填写。
  * 20171021: 增加在后端API未返回正确信息({"status": false})的情况下，提示使用另外的简介生成工具。并对于电影版提供简介美化及一键填写功能。
  * 20171014: 改用自己API来进行导入。增加搜索方法。
  * 20171012: Test Version~ , Thanks those Userscript:
