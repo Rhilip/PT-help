73f1bd893f1327479bff2f1aa1bf6cd5f8ca6815
Rhilip <rhilipruan@gmail.com>
2017-12-27 19:39:09 +0800

    REMOVE token which gen by projectpoi

--- modules/ptboard/__init__.py
+++ modules/ptboard/__init__.py
@@ -7,7 +7,6 @@ import time
 from flask import Blueprint, request, jsonify
 from app import mysql
 from pymysql import escape_string
-from modules.token import get_token_record, token_use
 
 ptboard_blueprint = Blueprint('ptboard', __name__)
 
@@ -41,73 +40,66 @@ def ptboard():
     }
 
     t0 = time.time()
-    token = request.args.get("token") or None
 
-    ret.update(get_token_record(token))
-    if ret.setdefault("success", False):
-        token_quote = int(ret.setdefault("quote", 0))
-        if token_quote is not 0:
+    search_raw = request.args.get("search") or search_default
+    order_raw = request.args.get("order") or order_default
+    site_raw = request.args.get("site") or site_default
+    no_site_raw = request.args.get("no_site") or no_site_default
+    limit = request.args.get("limit") or limit_default
+    offset = request.args.get("offset") or offset_default
+    start_time = request.args.get("start_time") or start_time_default
+    end_time = request.args.get("end_time") or end_time_default
 
-            search_raw = request.args.get("search") or search_default
-            order_raw = request.args.get("order") or order_default
-            site_raw = request.args.get("site") or site_default
-            no_site_raw = request.args.get("no_site") or no_site_default
-            limit = request.args.get("limit") or limit_default
-            offset = request.args.get("offset") or offset_default
-            start_time = request.args.get("start_time") or start_time_default
-            end_time = request.args.get("end_time") or end_time_default
+    search = re.sub(r"[ _\-,.+]", " ", search_raw)
+    search = search.split()
+    search = search[:10]
 
-            search = re.sub(r"[ _\-,.+]", " ", search_raw)
-            search = search.split()
-            search = search[:10]
+    search_opt = site_opt = no_site_opt = "1=1"
+    if search:
+        search_opt = warp_str(
+            " AND ".join(["`title` LIKE '%{key}%'".format(key=escape_string(i)) for i in search])
+        )
 
-            search_opt = site_opt = no_site_opt = "1=1"
-            if search:
-                search_opt = warp_str(
-                    " AND ".join(["`title` LIKE '%{key}%'".format(key=escape_string(i)) for i in search])
-                )
+    start_time = recover_int_to_default(start_time, start_time_default)
+    end_time = recover_int_to_default(end_time, end_time_default)
+    time_opt = warp_str("`pubDate` BETWEEN {start} AND {end}".format(start=start_time, end=end_time))
 
-            start_time = recover_int_to_default(start_time, start_time_default)
-            end_time = recover_int_to_default(end_time, end_time_default)
-            time_opt = warp_str("`pubDate` BETWEEN {start} AND {end}".format(start=start_time, end=end_time))
+    if site_raw:
+        site = site_raw.split(",")
+        site_opt = warp_str(
+            " OR ".join(["`site` = '{site}'".format(site=escape_string(s)) for s in site])
+        )
 
-            if site_raw:
-                site = site_raw.split(",")
-                site_opt = warp_str(
-                    " OR ".join(["`site` = '{site}'".format(site=escape_string(s)) for s in site])
-                )
+    if no_site_raw:
+        no_site = no_site_raw.split(",")
+        no_site_opt = warp_str(
+            " AND ".join(["`site` != '{site}'".format(site=escape_string(s)) for s in no_site])
+        )
 
-            if no_site_raw:
-                no_site = no_site_raw.split(",")
-                no_site_opt = warp_str(
-                    " AND ".join(["`site` != '{site}'".format(site=escape_string(s)) for s in no_site])
-                )
+    limit = recover_int_to_default(limit, limit_default)
+    offset = recover_int_to_default(offset, offset_default)
 
-            limit = recover_int_to_default(limit, limit_default)
-            offset = recover_int_to_default(offset, offset_default)
+    if limit > 200:
+        limit = 200
 
-            if limit > 200:
-                limit = 200
+    order = "desc" if order_raw not in ["desc", "asc"] else order_raw
 
-            order = "desc" if order_raw not in ["desc", "asc"] else order_raw
+    opt = " AND ".join([search_opt, time_opt, site_opt, no_site_opt])
+    sql = ("SELECT * FROM `api`.`ptboard_record` WHERE {opt} ORDER BY `pubDate` {_da} "
+           "LIMIT {_offset}, {_limit}".format(opt=opt, _da=order.upper(), _offset=offset, _limit=limit)
+           )
 
-            opt = " AND ".join([search_opt, time_opt, site_opt, no_site_opt])
-            sql = ("SELECT * FROM `api`.`ptboard_record` WHERE {opt} ORDER BY `pubDate` {_da} "
-                   "LIMIT {_offset}, {_limit}".format(opt=opt, _da=order.upper(), _offset=offset, _limit=limit)
-                   )
+    record_count, rows_data = mysql.exec(sql=sql, r_dict=True, fetch_all=True, ret_row=True)
 
-            record_count, rows_data = mysql.exec(sql=sql, r_dict=True, fetch_all=True, ret_row=True)
+    total_data = mysql.exec("SELECT `TABLE_ROWS` FROM `information_schema`.`TABLES` "
+                            "WHERE `TABLE_NAME`='ptboard_record'")[0]
 
-            total_data = mysql.exec("SELECT `TABLE_ROWS` FROM `information_schema`.`TABLES` "
-                                    "WHERE `TABLE_NAME`='ptboard_record'")[0]
-
-            ret.update({
-                "success": True,
-                "quote": token_quote,
-                "rows": rows_data,
-                "total": record_count if search else total_data,
-            })
-            ret.update(token_use(token))
+    ret.update({
+        "success": True,
+        # "quote": token_quote,
+        "rows": rows_data,
+        "total": record_count if search else total_data,
+    })
 
     ret.update({"cost": time.time() - t0})
     return jsonify(ret)
