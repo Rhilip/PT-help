b70a7d5157aa842861b7095a98d687113423e067
Rhilip <rhilipruan@gmail.com>
2018-02-11 22:16:21 +0800

    add backtracking script for NexusPHP

--- modules/ptboard/db/ptboard_record.sql
+++ modules/ptboard/db/ptboard_record.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 2018-01-01 11:53:49
--- 服务器版本： 5.6.37-log
+-- Generation Time: 2018-01-31 13:13:34
+-- 服务器版本： 5.6.39-log
 -- PHP Version: 7.0.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -12,11 +12,25 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+--
+-- Database: `api`
+--
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ptboard_recheck`
+--
+
+CREATE TABLE `ptboard_recheck` (
+  `rid`   INT(11) NOT NULL,
+  `sid`   INT(11) NOT NULL,
+  `site`  TEXT    NOT NULL,
+  `title` TEXT    NOT NULL,
+  `link`  TEXT    NOT NULL
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -25,12 +39,55 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `ptboard_record` (
-  `uid`     INT(11) NOT NULL,
-  `sid`     INT(11) NOT NULL,
-  `site`    TEXT    NOT NULL,
-  `title`   TEXT    NOT NULL,
-  `link`    TEXT    NOT NULL,
-  `pubDate` INT(11) NOT NULL
+  `uid`     INT(11)     NOT NULL,
+  `sid`     INT(11)     NOT NULL,
+  `site`    VARCHAR(20) NOT NULL,
+  `title`   TEXT        NOT NULL,
+  `link`    TEXT        NOT NULL,
+  `pubDate` INT(11)     NOT NULL
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8mb4;
+
+--
+-- 触发器 `ptboard_record`
+--
+DELIMITER $$
+CREATE TRIGGER `Auto_Update_ptboard_site`
+AFTER INSERT ON `ptboard_record`
+FOR EACH ROW
+  BEGIN
+    DECLARE s VARCHAR(20);
+    SET s = (SELECT `site`
+             FROM `ptboard_record`
+             ORDER BY `uid` DESC
+             LIMIT 1);
+    UPDATE `ptboard_site`
+    SET `last_check_id` = (SELECT MAX(`sid`)
+                           FROM `api`.`ptboard_record`
+                           WHERE `site` = s), `count_id` = (SELECT COUNT(`sid`)
+                                                            FROM `api`.`ptboard_record`
+                                                            WHERE `site` = s)
+    WHERE `site` = s;
+  END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `ptboard_site`
+--
+
+CREATE TABLE `ptboard_site` (
+  `sid`             INT(11)                                 NOT NULL,
+  `site`            TEXT                                    NOT NULL,
+  `url`             TEXT                                    NOT NULL,
+  `status`          TINYINT(1)                              NOT NULL DEFAULT '1',
+  `type`            ENUM ('rss', 'scrapy', 'other', 'none') NOT NULL DEFAULT 'rss',
+  `last_check_id`   INT(11)                                 NOT NULL DEFAULT '0',
+  `count_id`        INT(11)                                 NOT NULL,
+  `last_check_time` DATETIME                                NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )
   ENGINE = MyISAM
   DEFAULT CHARSET = utf8mb4;
@@ -40,20 +97,38 @@ CREATE TABLE `ptboard_record` (
 --
 
 --
+-- Indexes for table `ptboard_recheck`
+--
+ALTER TABLE `ptboard_recheck`
+  ADD KEY `rid` (`rid`);
+
+--
 -- Indexes for table `ptboard_record`
 --
 ALTER TABLE `ptboard_record`
   ADD PRIMARY KEY (`uid`),
-  ADD KEY `pubDate` (`pubDate`),
-  ADD KEY `sid` (`sid`);
+  ADD UNIQUE KEY `unique_sid_in_one_site` (`sid`, `site`),
+  ADD KEY `pubDate` (`pubDate`);
 ALTER TABLE `ptboard_record`
   ADD FULLTEXT KEY `title` (`title`);
-ALTER TABLE `ptboard_record`
-  ADD FULLTEXT KEY `site` (`site`);
+
+--
+-- Indexes for table `ptboard_site`
+--
+ALTER TABLE `ptboard_site`
+  ADD PRIMARY KEY (`sid`),
+  ADD KEY `status` (`status`),
+  ADD KEY `type` (`type`);
 
 --
 -- 在导出的表使用AUTO_INCREMENT
 --
+
+--
+-- 使用表AUTO_INCREMENT `ptboard_recheck`
+--
+ALTER TABLE `ptboard_recheck`
+  MODIFY `rid` INT(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- 使用表AUTO_INCREMENT `ptboard_record`
@@ -61,7 +136,3 @@ ALTER TABLE `ptboard_record`
 ALTER TABLE `ptboard_record`
   MODIFY `uid` INT(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
--- modules/ptboard/script/backtracking.py
+++ modules/ptboard/script/backtracking.py
@@ -4,13 +4,17 @@
 
 """
 这是一个用来"回溯"爬取NexusPHP构架的PT站点,并从中获取种子信息（种子序号、种子名、种子链接、发布时间）
-本脚本直接爬取种子列表页，属于“快速爬取”，仅适用于对原构架改动不大的站点，如需按id全部点取或不适应的话，请使用另一脚本。
+仅作示例！！！！！！！！！！！！！
 
 请注意：食用前请先配置好依赖并填好配置项，并将对应站点的时间类型改为“发生时间”
+建议的食用顺序为：RSS >> list >>>>> id
 """
 
 import re
 import time
+import feedparser
+import dateutil.parser
+from html import unescape
 from http.cookies import SimpleCookie
 
 import requests
@@ -25,18 +29,44 @@ db_password = "password"
 db_db = "db"
 
 site_config = [
+    # # 这是按照RSS源爬取的配置
+    {
+        "name": "NexusPHP",  # 站点名称
+        "rss_url": "https://nexusphp.com/torrentrss.php",  # 站点RSS地址（如需passkey认证请带上）
+        "rows": 50,  # 单次请求数
+        "max_torrent": 125, # 该站点最大种子号
+        "start_torrent": 0,  # 种子起始范围（仅用来限定，适用于爬取到一半进程停止的情况重爬）
+        "end_torrent": 9999999,  # 种子结束范围（仅用来限定）
+        "search_type": "rss",  # 回爬方式 ENUM("list", "id", "rss")
+        # "enable": True  # 如果启用该方法，请取消该注释
+    },
+    # # 这是按照种子列表页爬取的配置（注意将站点的时间显示方式改为"发生时间"）
     {
         "name": "NexusPHP",  # 站点名称
         "page_url": "https://nexusphp.com/torrents.php?incldead=0&page={}",  # 站点包含死种的分页地址，请请将分页用{}替代
         "torrent_url": "https://nexusphp.com/details.php?id={}&hit=1",  # 站点种子页面，请将种子id用{}替代
         "start_page": 0,  # 抓取起始页
-        "end_page": 20,  # 抓取结束页（请使用站点最后一页的分页号）
+        "end_page": 2,  # 抓取结束页（请使用站点最后一页的分页号）
         "start_torrent": 0,  # 种子起始范围（仅用来限定，适用于爬取到一半进程停止的情况重爬）
         "end_torrent": 9999999,  # 种子结束范围（仅用来限定）
-        "cookies": ""  # 站点Cookies
+        "cookies": "",  # 站点Cookies
+        "search_type": "list",  # 回爬方式
+        # "enable": True,    # 如果启用该方法，请取消该注释
     },
-    # 另一站点信息
+    # # 这是按照种子详情页爬取的配置
+    {
+        "name": "NexusPHP",  # 站点名称
+        "torrent_url": "https://nexusphp.com/details.php?id={}&hit=1",  # 站点种子页面，请将种子id用{}替代
+        "start_torrent": 0,  # 种子起始范围
+        "end_torrent": 999,  # 种子结束范围
+        "title_ptn": "title>.+?(&quot;)?(?P<title>.+)(&quot;)? - Powered by NexusPHP",  # 种子页面标题信息中的主标题信息
+        "cookies": "",  # 站点Cookies
+        "search_type": "id",  # 回爬方式
+        # "enable": True,    # 如果启用该方法，请取消该注释
+    },
 ]
+
+
 
 # -*- 结束配置
 
@@ -48,12 +78,7 @@ cursor = db.cursor()
 headers = {
     'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
 }
-
-insert_sql = (
-    "INSERT INTO `ptboard_record` (`sid`,`site`,`title`,`link`,`pubDate`) "
-    "VALUES ('{sid}','{site}', '{title}','{link}', '{pubDate}')"
-)
-
+feedparser.USER_AGENT = 'FlexGet/2.10.61 (www.flexget.com)'
 
 def cookies_raw2jar(raw: str) -> dict:
     """
@@ -80,68 +105,122 @@ def string_sort(string):
     return pymysql.escape_string(string)
 
 
-if __name__ == "__main__":
-    for site in site_config:
-        # 构造站点Cookies
-        cookies = cookies_raw2jar(site['cookies'])
+insert_sql = (
+    "INSERT INTO `ptboard_record` (`sid`,`site`,`title`,`link`,`pubDate`) "
+    "VALUES ('{sid}','{site}', '{title}','{link}', '{pubDate}')"
+)
 
-        for page in range(site['start_page'], site['end_page'] + 2):
-            t0 = time.time()
 
-            error_get = 0
-            while error_get < 4:
-                try:  # 获取种子列表页
-                    list_page = requests.get(site['page_url'].format(page), cookies=cookies, headers=headers)
-                except OSError:  # 防止站点无响应
-                    error_get += 1
-                    time.sleep((error_get + 1) ** 2)
+def wrap_insert(site, sid, title, link, pubdate, t):
+    try:
+        cursor.execute(insert_sql.format(sid=sid, site=site, title=title, link=link, pubDate=pubdate))
+    except pymysql.Error:
+        pass
+    else:
+        print("Site: {}, ID: {}, Cost: {:.5f} s, "
+              "pubDate: {}, Title: {}".format(site, sid, time.time() - t, pubdate, title))
+
+
+def backtracking_list(site):
+    cookies = cookies_raw2jar(site['cookies'])
+    for page in range(site['start_page'], site['end_page'] + 2):
+        t0 = time.time()
+
+        list_page = requests.get(site['page_url'].format(page), cookies=cookies, headers=headers)
+        list_page.encoding = "utf-8"  # 修改编码
+        list_page_bs = BeautifulSoup(list_page.text, "lxml")  # 使用BeautifulSoup库解析页面
+
+        tid_list = list_page_bs.find_all(has_title)
+
+        for tid_tag in tid_list:
+            # tid_tag = tid_list[0]
+            t1 = time.time()
+
+            # 种子编号
+            _tid = int(re.search("details\.php\?id=(?P<id>\d+)&hit=1", tid_tag["href"]).group("id"))
+            if _tid in range(site['start_torrent'], site['end_torrent']):
+            # 种子标题
+                try:
+                    _title = tid_tag["title"]
+                    # title = tid_tag.parent.get_text(" | ", strip=True)
+                    # title = re.sub(re.escape(" | [ | 热门 | ]"), "", title)
+                except KeyError:
+                    _title = tid_tag.text
+                _title = string_sort(_title)
+
+                # 种子链接
+                _link = site['torrent_url'].format(_tid)
+
+                # 种子发布时间
+                _timestamp = 0
+                for i in tid_tag.parent.parent.parent.parent.parent.find_all("td", class_="nowrap"):
+                    try:  # ↑ 此处（parent层级）可能出错
+                        pubDate_raw = time.strptime(i.get_text(" "), "%Y-%m-%d %H:%M:%S")
+                    except ValueError:
+                        pass
+                    else:
+                        _timestamp = int(time.mktime(pubDate_raw))
+                        break
+
+                wrap_insert(site=site['name'], sid=_tid, title=_title, link=_link, pubdate=_timestamp, t=t1)
+
+        print("Pages: {p} Over, Cost: {t:.5f} s".format(p=page, t=time.time() - t0))
+
+        time.sleep(5)
+
+
+def backtracking_id(site):
+    cookies = cookies_raw2jar(site['cookies'])
+    for _tid in range(site['start_torrent'], site['end_torrent'] + 2):
+        t0 = time.time()
+
+        _link = site['torrent_url'].format(_tid)
+        torrent_page = requests.get(_link, cookies=cookies, headers=headers)
+        title_search = re.search(site['search_ptn'], torrent_page.text)
+
+        if title_search:
+            _title = pymysql.escape_string(unescape(title_search.group("title")))
+            pubDate = re.search("发布于(.+?)<", torrent_page.text).group(1)
+            _timestamp = time.mktime(time.strptime(pubDate, "%Y-%m-%d %H:%M:%S"))
+
+            wrap_insert(site=site['name'], sid=_tid, title=_title, link=_link, pubdate=_timestamp, t=t0)
+        else:
+            print("ID: {}, Cost: {:.5f} s, No torrent.".format(_tid, time.time() - t0))
+
+        time.sleep(2)
+
+def backtracking_rss(site):
+    for startindex in range(0, site['max_torrent'] + site['rows'], site['rows']):
+        feed_data = requests.get(site["rss_url"],params={'rows': site['rows'], 'startindex': startindex})
+        print(feed_data.url)
+        feed = feedparser.parse(feed_data.text)
+        feed_entries = feed.entries
+        if feed_entries:
+            for item in feed_entries:
+                t0 = time.time()
+                _tid = int(re.search("(id=|(torrents|t)/)(?P<id>\d+)", item.link).group("id"))
+
+                _title = string_sort(item.title)
+                if hasattr(item, "published"):
+                    _timestamp = dateutil.parser.parse(item.published).timestamp()
                 else:
-                    list_page.encoding = "utf-8"  # 修改编码
-                    list_page_bs = BeautifulSoup(list_page.text, "lxml")  # 使用BeautifulSoup库解析页面
+                    if site['name'] == "CCFBits":
+                        time_string_from_des = re.search("(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})", item.summary)
+                        time_string = time_string_from_des.group(1) + " +0000"
+                        _timestamp = dateutil.parser.parse(time_string).timestamp()
+                    else:
+                        _timestamp = time.time()
 
-                    tid_list = list_page_bs.find_all(has_title)
+                    # re.sub("&?passkey=[0-9a-zA-Z]{32}", "", item.link)
 
-                    for tid_tag in tid_list:
-                        # tid_tag = tid_list[0]
-                        t1 = time.time()
-                        tid = int(re.search("details\.php\?id=(?P<id>\d+)&hit=1", tid_tag["href"]).group("id"))  # 种子编号
+                wrap_insert(site=site['name'], sid=_tid, title=_title, link=item.link, pubdate=_timestamp, t=t0)
+        else:
+            break
 
-                        if tid in range(site['start_torrent'], site['end_torrent']):
-                            # 种子标题
-                            try:
-                                title = tid_tag["title"]
-                                # title = tid_tag.parent.get_text(" | ", strip=True)
-                                # title = re.sub(re.escape(" | [ | 热门 | ]"), "", title)
-                            except KeyError:
-                                title = tid_tag.text
-                            title = string_sort(title)
+if __name__ == "__main__":
+    t = time.time()
+    for site in site_config:
+        if site.get("enable", False):
+            locals()["backtracking_" + site["search_type"]](site)
 
-                            # 种子链接
-                            link = site['torrent_url'].format(tid)
-
-                            # 种子发布时间
-                            timestamp = 0
-                            for i in tid_tag.parent.parent.parent.parent.parent.find_all("td", class_="nowrap"):
-                                try:
-                                    pubDate_raw = time.strptime(i.get_text(" "), "%Y-%m-%d %H:%M:%S")
-                                except ValueError:
-                                    pass
-                                else:
-                                    timestamp = int(time.mktime(pubDate_raw))
-                                    break
-
-                            sql = insert_sql.format(sid=tid, site=site['name'], title=title,
-                                                    link=link, pubDate=timestamp)
-
-                            try:
-                                cursor.execute(sql)
-                            except pymysql.Error:
-                                pass
-                            else:
-                                print("Page: {}, ID: {}, Cost: {:.5f} s, "
-                                      "pubDate: {}, Title: {}".format(page, tid, time.time() - t1, timestamp, title))
-
-            print("Pages: {p} Over, Cost: {t:.5f} s".format(p=page, t=time.time() - t0))
-            time.sleep(5)
-
-    print("END AT: {}".format(time.asctime(time.localtime(time.time()))))
+    print("END AT: {}, Total Cost {:.5f} s".format(time.asctime(time.localtime(time.time())), time.time() - t))
