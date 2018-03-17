bb5f86945135fac2abba4afc1ea17e781445f2b7
Rhilip <rhilipruan@gmail.com>
2018-03-17 12:34:38 +0800

    Add time cost

--- modules/infogen/__init__.py
+++ modules/infogen/__init__.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2017-2020 Rhilip <rhilipruan@gmail.com>
 
 import json
+import time
 
 from pymysql import escape_string
 from flask import Blueprint, request, jsonify, redirect
@@ -23,14 +24,15 @@ def gen():
         url = ""
 
     if url:
+        t0 = time.time()
         _gen = Gen(url=url)
 
-        row, data = mysql.exec(
+        row, db_data = mysql.exec(
             "SELECT * FROM `api`.`gen_info` WHERE `site`='{}' AND `sid`='{}'".format(_gen.site, _gen.sid),
             r_dict=True, ret_row=True
         )
 
-        if int(row) == 0:
+        if int(row) == 0:  # 数据库无该缓存数据
             data = _gen.gen()
             if data["success"]:  # 正确获取的情况下缓存请求结果
                 mysql.exec(
@@ -38,9 +40,9 @@ def gen():
                     " VALUES ('{}', '{}', '{}')".format(_gen.site, _gen.sid, escape_string(json.dumps(data)))
                 )
         else:
-            data_str = data.get("data")
-            data = json.loads(data_str)
+            data = json.loads(db_data.get("data"))
 
+        data.update({"cost": time.time() - t0})
         return jsonify(data)
     else:
         return redirect("https://git.io/vFvmP", code=301)
